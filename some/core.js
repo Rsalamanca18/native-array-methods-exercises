7ad8667f7814d966f3f1672615390c3a05fc1b89
Rodrigo Salamanca <salamanca_rod18@yahoo.com>
2018-08-18 11:48:39 -0700

    some functions done

--- some/core.js
+++ some/core.js
@@ -2,28 +2,44 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+
+  // input.some(item => item > 10)
+
+  return input.some(function(item){
+    return item > 10
+  })
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(function(item){
+    return item.length > 10
+  })
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.some(function(item, i){
+    if (item[i]=== true){
+      return item
+    }
+  })
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  return input.some(function(item, i){
+    let word = 'Lost'
+    if (item.includes(word)){
+      return item
+    }
+  })
 };
 
 module.exports = {
